9fb821a0d7d4760f84e9ca46fba4a7f067c2f15e
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2021-12-19 21:38:34 -0500

    Added Button to search bar

--- src/Search.js
+++ src/Search.js
@@ -4,28 +4,40 @@ import "./Search.css";
 export default function Search() {
   return (
     <div className="Search">
+      {/* Search bar container */}
       <div className="container">
         <div className="row">
           <div className="col-sm">
+            {/* Form */}
             <form>
               <div className="row">
                 <div className="col-md-3 d-md-block d-none search-bar"></div>
-                <div className="col-md-3 d-md-block d-none search-bar">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="First name"
-                    aria-label="First name"
-                  />
+                {/* Search bar (displayed for desktop) */}
+                <div className="input-group">
+                  <div className="col-md-3 d-md-block d-none search-bar">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Type a city"
+                      aria-label="Type a city"
+                    />
+                  </div>
+                  <div className="col-md-3 d-md-block d-none search-bar">
+                    Hey
+                  </div>
                 </div>
                 <div className="col-2 d-md-none d-block search-bar"></div>
+                {/* Search bar (displayed for mobile) */}
                 <div className="col-5 d-md-none d-block search-bar">
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="First name"
-                    aria-label="First name"
+                    placeholder="Type a city"
+                    aria-label="Type a city"
                   />
+                  <button type="submit" class="btn btn-primary">
+                    Sign in
+                  </button>
                 </div>
               </div>
             </form>
