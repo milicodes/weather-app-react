ea1ff7b3a473b8006b294ea407c23e60cdc0c323
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2021-12-20 21:41:52 -0500

    Created responsive button

--- src/Search.js
+++ src/Search.js
@@ -28,18 +28,22 @@ export default function Search() {
                     </button>
                   </div>
                 </div>
-                <div className="col-2 d-md-none d-block search-bar"></div>
-                {/* Search bar (displayed for mobile) */}
-                <div className="col-5 d-md-none d-block search-bar">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Type a city"
-                    aria-label="Type a city"
-                  />
-                  <button type="submit" class="btn btn-primary">
-                    Sign in
-                  </button>
+                <div className="input-group">
+                  <div className="col-3 d-md-none d-block search-bar"></div>
+                  {/* Search bar (displayed for mobile) */}
+                  <div className="col-5 d-md-none d-block search-bar">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Type a city"
+                      aria-label="Type a city"
+                    />
+                  </div>
+                  <div className="col-2 d-md-none d-block search-bar">
+                    <button type="submit" class="btn btn-primary">
+                      Search
+                    </button>
+                  </div>
                 </div>
               </div>
             </form>
