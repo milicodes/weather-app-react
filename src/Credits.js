87cddf3620f9a1c3aac4e1b603a3a73ab276a23a
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-01-17 01:25:34 -0500

    Added link to github

--- src/Credits.js
+++ src/Credits.js
@@ -3,12 +3,24 @@ import "./Credits.css"
 
 export default function Credits() {
     return (
-      <div className="Credits col-bor">
+      <div className="Credits">
         <div className="container">
           <div className="row">
-              <div className="col-12">
-                <h6 className="credits-section col-bor"> Coded by Mili Pernia, open sourced on Github. Hosted on Netlify</h6>
-              </div>
+            <div className="col-12">
+              <h6 className="credits-section">
+                {" "}
+                Coded by Mili Pernia,{" "}
+                <a
+                  className="link-github"
+                  href="https://github.com/milicodes/weather-app-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  open sourced on Github.
+                </a>
+                Hosted on Netlify
+              </h6>
+            </div>
           </div>
         </div>
       </div>
