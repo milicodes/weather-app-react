c8949b9d079461d8f42df09baf90c8442caf7c5c
Milagros Pernia Gonzalez <starrymiracles@gmail.com>
2022-01-17 23:15:12 -0500

    Added animations to the rest of the forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,9 +2,7 @@ import React from "react";
 import "./Forecast.css";
 import Anime from "react-anime";
 
-
 export default function Forecast() {
-
   return (
     <div className="container">
       <div className="row">
@@ -29,49 +27,94 @@ export default function Forecast() {
         </div>
 
         <div className="col-2">
-          <div className="container forecast-container day-two">
-            <div className="row justify-content-center">
-              <h4 className="col-12 day-forecast-top">TUE</h4>
-              <h4 className="col-12 icon-forecast">O</h4>
-              <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+          <Anime
+            className="animation-mobile"
+            easing="easeOutElastic"
+            direction="alternate"
+            loop={true}
+            delay={(el, index) => index * 240}
+            translateY={-14}
+          >
+            <div className="container forecast-container day-two">
+              <div className="row justify-content-center">
+                <h4 className="col-12 day-forecast-top">TUE</h4>
+                <h4 className="col-12 icon-forecast">O</h4>
+                <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+              </div>
             </div>
-          </div>
+          </Anime>
         </div>
         <div className="col-2">
-          <div className="container forecast-container day-three">
-            <div className="row justify-content-center">
-              <h4 className="col-12 day-forecast-top">WED</h4>
-              <h4 className="col-12 icon-forecast">O</h4>
-              <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+          <Anime
+            className="animation-mobile"
+            easing="easeOutElastic"
+            direction="alternate"
+            loop={true}
+            delay={(el, index) => index * 240}
+            translateY={-14}
+          >
+            <div className="container forecast-container day-three">
+              <div className="row justify-content-center">
+                <h4 className="col-12 day-forecast-top">WED</h4>
+                <h4 className="col-12 icon-forecast">O</h4>
+                <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+              </div>
             </div>
-          </div>
+          </Anime>
         </div>
         <div className="col-2">
-          <div className="container forecast-container day-four">
-            <div className="row justify-content-center">
-              <h4 className="col-12 day-forecast-top">THU</h4>
-              <h4 className="col-12 icon-forecast">O</h4>
-              <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+          <Anime
+            className="animation-mobile"
+            easing="easeOutElastic"
+            direction="alternate"
+            loop={true}
+            delay={(el, index) => index * 240}
+            translateY={-14}
+          >
+            <div className="container forecast-container day-four">
+              <div className="row justify-content-center">
+                <h4 className="col-12 day-forecast-top">THU</h4>
+                <h4 className="col-12 icon-forecast">O</h4>
+                <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+              </div>
             </div>
-          </div>
+          </Anime>
         </div>
         <div className="col-2">
-          <div className="container forecast-container day-five">
-            <div className="row justify-content-center">
-              <h4 className="col-12 day-forecast-top">FRI</h4>
-              <h4 className="col-12 icon-forecast">O</h4>
-              <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+          <Anime
+            className="animation-mobile"
+            easing="easeOutElastic"
+            direction="alternate"
+            loop={true}
+            delay={(el, index) => index * 240}
+            translateY={-14}
+          >
+            <div className="container forecast-container day-five">
+              <div className="row justify-content-center">
+                <h4 className="col-12 day-forecast-top">FRI</h4>
+                <h4 className="col-12 icon-forecast">O</h4>
+                <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+              </div>
             </div>
-          </div>
+          </Anime>
         </div>
         <div className="col-2">
-          <div className="container forecast-container day-six">
-            <div className="row justify-content-center">
-              <h4 className="col-12 day-forecast-top">SAT</h4>
-              <h4 className="col-12 icon-forecast">O</h4>
-              <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+          <Anime
+            className="animation-mobile"
+            easing="easeOutElastic"
+            direction="alternate"
+            loop={true}
+            delay={(el, index) => index * 240}
+            translateY={-14}
+          >
+            <div className="container forecast-container day-six">
+              <div className="row justify-content-center">
+                <h4 className="col-12 day-forecast-top">SAT</h4>
+                <h4 className="col-12 icon-forecast">O</h4>
+                <h5 className="col-12 day-forecast-bot">23C / 9C</h5>
+              </div>
             </div>
-          </div>
+          </Anime>
         </div>
       </div>
     </div>
